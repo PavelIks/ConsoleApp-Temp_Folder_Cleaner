2696e6b23084bd9e409bb3eee2cd9efa4ace21ef
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-02 00:02:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace TempFolderCeaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var Directorys in Directory.GetDirectories($@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\Temp\"))
+            {
+                try
+                {
+                    Directory.Delete(Directorys.ToString(), true);
+                    Console.WriteLine("true (Удалено)", Console.ForegroundColor = ConsoleColor.DarkGreen);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("false (Не подлежит удалению)", Console.ForegroundColor = ConsoleColor.DarkRed);
+                }
+            }
+            foreach (var Files in Directory.GetFiles($@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\Temp\"))
+            {
+                try
+                {
+                    File.Delete(Files.ToString());
+                    Console.WriteLine("true (Удалено)", Console.ForegroundColor = ConsoleColor.DarkGreen);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("false (Не подлежит удалению)", Console.ForegroundColor = ConsoleColor.DarkRed);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
